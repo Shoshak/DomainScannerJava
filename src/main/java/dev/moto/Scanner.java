1bf6178d46d003675bdcd2eab56d07415075a6ab
Shoshak <motorealinfo@gmail.com>
2024-09-30 13:25:52 +0500

    Fix list race condition

--- src/main/java/dev/moto/Scanner.java
+++ src/main/java/dev/moto/Scanner.java
@@ -11,22 +11,34 @@ import java.util.List;
 public class Scanner {
     private final Connection connection;
     private final DefaultListModel<String> model;
+    private int handling;
 
     public Scanner(DefaultListModel<String> model) {
         this.connection = Jsoup.newSession().timeout(5000);
         this.model = model;
+        this.handling = 0;
     }
 
     public void scan(List<String> domains, String siteName) throws URISyntaxException, IOException {
+        this.handling++;
+        int currentHandling = this.handling;
         for (String domain : domains) {
             String link = siteName + "." + domain;
             Thread.ofVirtual().start(() -> {
                 try {
                     String title = this.connection.newRequest("http://" + link).get().title();
                     if (!title.isEmpty()) {
-                        SwingUtilities.invokeLater(() -> model.addElement(String.format("%s (%s)", link, title)));
+                        SwingUtilities.invokeLater(() -> {
+                            if (currentHandling == this.handling) {
+                                model.addElement(String.format("%s (%s)", link, title));
+                            }
+                        });
                     } else {
-                        SwingUtilities.invokeLater(() -> model.addElement(link));
+                        SwingUtilities.invokeLater(() -> {
+                            if (currentHandling == this.handling) {
+                                model.addElement(link);
+                            }
+                        });
                     }
                 } catch (IOException ignored) {}
             });
